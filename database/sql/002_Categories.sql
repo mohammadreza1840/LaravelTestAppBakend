21467ca76602026e46cb55cce41b6537cbfe03ad
mohammadreza1840 <mohammad.reza.mahdavi1840@gmail.com>
2023-02-08 22:53:26 +0330

    add sql files and images in laravel

---
+++ database/sql/002_Categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("مهارت های زندگی", 6);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("خانواده", 1);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("شخصیت", 5);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("مراحل رشد", 2);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("مراحل غی", 2);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("شکوفایی استعداد", 2);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("اخلاق", 4);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("آسیب های فردی", 3);
+INSERT INTO `categories` (`title`, `extra_data_id`) VALUES ("منجی و انتظار", 2);
