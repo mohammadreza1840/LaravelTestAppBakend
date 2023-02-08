e139c322ce47aaa9f5cbaae6d53d5c6dda828aa3
mohammadreza1840 <mohammad.reza.mahdavi1840@gmail.com>
2023-02-09 02:01:09 +0330

    change extradata_id to iamge_source in Tests Resource

--- app/Http/Resources/Ver1/Tests.php
+++ app/Http/Resources/Ver1/Tests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Ver1;
 
 use App\Http\Resources\ver1\QuestionCollection;
+use App\Models\ExtraData;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Tests extends JsonResource
@@ -21,7 +22,7 @@ class Tests extends JsonResource
             'longDescriptions'=>$this->longDescriptions,
             'isActive'=>$this->isActive,
             'category_id'=>$this->category_id,
-            'extradata_id'=>$this->extradata_id,
+            'image_source'=>ExtraData::find($this->extra_data_id)->source,
             'created_at'=>$this->created_at,
             'updated_at'=>$this->updated_at,
             'questions'=> new QuestionCollection($this->questions),
