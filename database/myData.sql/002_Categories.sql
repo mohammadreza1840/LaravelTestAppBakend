04f3f8cefa831d6f21d37513cd11b73cdc121be4
mohammadreza1840 <mohammad.reza.mahdavi1840@gmail.com>
2023-02-10 00:10:16 +0330

    add mydata.sql

---
+++ database/myData.sql/002_Categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('مهارت های زندگی', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('خانواده', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('شخصیت', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('مراحل رشد (صعود) فرد بر اساس استعداد', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('مراحل غی (سقوط) فرد', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('شکوفایی استعداد', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('اخلاق', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('آسیب های فردی', NULL);
+INSERT INTO "Categories" ("Title", "ImageID") VALUES ('منجی و انتظار', NULL);
